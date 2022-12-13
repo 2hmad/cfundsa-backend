dc613c8ae475bc92f1e3fc09b279db0021e2ea2d
Ahmed Mohamed Ibrahim <ahmed_noreldin@outlook.com>
2022-12-13 23:59:30 +0200

    limit waiting dates

--- app/Http/Controllers/WaitingDatesController.php
+++ app/Http/Controllers/WaitingDatesController.php
@@ -15,15 +15,15 @@ class WaitingDatesController extends Controller
     public function getWaitingDatesByDate($date)
     {
         if ($date == 'today') {
-            return Appointments::where('date', date('Y-m-d'))->with('company')->get();
+            return Appointments::where('date', date('Y-m-d'))->with('company')->limit(5)->get();
         } else if ($date == 'week') {
             $start = date('Y-m-d');
             $end = date('Y-m-d', strtotime('-7 days'));
             // get appointments when date from start to end date
 
-            return Appointments::whereBetween('date', [$end, $start])->with('company')->get();
+            return Appointments::whereBetween('date', [$end, $start])->with('company')->limit(5)->get();
         } else if ($date == 'month') {
-            return Appointments::whereMonth('date', date('m'))->with('company')->get();
+            return Appointments::whereMonth('date', date('m'))->with('company')->limit(5)->get();
         }
     }
 }
